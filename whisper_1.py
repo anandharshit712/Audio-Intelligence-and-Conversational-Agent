22a2255c6ab245918d06f39923fa5e2374c491a8
Harshit Anand <anandharshit560@gmail.com>
2024-09-05 21:25:53 +0530

    Commit 23

--- whisper_1.py
+++ whisper_1.py
@@ -4,11 +4,9 @@ model = whisper.load_model('large')
 
 def transcribe(audio_file, detected_language):
     if detected_language:
-        result = model.transcribe("temp.wav")
+        result = model.transcribe(audio_file)
         transcription = result['text']
         return transcription
     else:
         print("Language detection failed; skipping transcription and translation.")
         return None
-
-print(transcribe("temp.wav", "en"))
